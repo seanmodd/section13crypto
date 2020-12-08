4b0aea71efc6685102d0c77f72bc32867f62b798
Sean Modd <seanmodd@Seans-MacBook-Pro.local>
2020-12-08 15:38:52 -0800

    index js and index html commit

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
-app.get("/", function (req, res) {
-  res.send("Hello World2");
-});
+app.use(express.static(__dirname + "/public"));
 app.listen(3000);
